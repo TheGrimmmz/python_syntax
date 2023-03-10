faebf88342c64221f9f0daab7d830eda25f8f062
Nick Mitchell <119639651+TheGrimmmz@users.noreply.github.com>
2023-03-09 18:23:18 -0600

    forgot docstring

--- words.py
+++ words.py
@@ -1,12 +1,12 @@
 def print_upper_words(words):
-
+    """Print words in uppercase"""
     for word in words:
         print(word.upper())
 
 print_upper_words(['magnetic', 'electromagnetic', 'difuser'])
 
 def print_upper_words_e(words):
-
+    """Print words in uppercase and start with e or E"""
     for word in words:
         if word.startswith('e') or word.startswith('E'):
             print(word.upper())
@@ -15,7 +15,7 @@ print_upper_words_e(['magnetic', 'electromagnetic', 'difuser', 'elon'])
 
 
 def print_upper_words_specific(words, must_start_with):
-
+    """Print words in uppercase and with words that start with m and e"""
     for word in words:
         for letter in must_start_with:
             if word.startswith(letter):
